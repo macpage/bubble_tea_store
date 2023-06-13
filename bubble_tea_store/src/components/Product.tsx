5ac168675a569dc5c23b54565aafdb7621138337
Mac <sophanpa@gmail.com>
2023-06-13 23:55:44 +0200

    Edit Product

--- bubble_tea_store/src/components/Product.tsx
+++ bubble_tea_store/src/components/Product.tsx
@@ -2,11 +2,7 @@ function Product(props) {
   return (
     <div className="product">
       <img src="" alt="" />
-      <div className="info">
-        <p>{props.title}</p>
-        <p>{props.price}</p>
-        <button>{props.button}</button>
-      </div>
+      <div className="info"></div>
     </div>
   );
 }
