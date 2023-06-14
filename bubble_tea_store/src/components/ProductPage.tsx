3f0f78cc4d47a771ccf0eaaace4246102f6f727f
Mac <sophanpa@gmail.com>
2023-06-14 23:41:02 +0200

    Add img

--- bubble_tea_store/src/components/ProductPage.tsx
+++ bubble_tea_store/src/components/ProductPage.tsx
@@ -3,15 +3,15 @@ import Product from './Product';
 function ProductPage() {
   return (
     <div id="product_page">
-      <Product title="Brown Sugar" price="4.99€" button="Add"></Product>
-      <Product title="Brown Sugar" price="4.99€" button="Add"></Product>
-      <Product title="Brown Sugar" price="4.99€" button="Add"></Product>
-      <Product title="Brown Sugar" price="4.99€" button="Add"></Product>
-      <Product title="Brown Sugar" price="4.99€" button="Add"></Product>
-      <Product title="Brown Sugar" price="4.99€" button="Add"></Product>
-      <Product title="Brown Sugar" price="4.99€" button="Add"></Product>
-      <Product title="Brown Sugar" price="4.99€" button="Add"></Product>
-      <Product title="Brown Sugar" price="4.99€" button="Add"></Product>
+      <Product img="" title="Brown Sugar" price="4.99€" button="Add"></Product>
+      <Product img="" title="Brown Sugar" price="4.99€" button="Add"></Product>
+      <Product img="" title="Brown Sugar" price="4.99€" button="Add"></Product>
+      <Product img="" title="Brown Sugar" price="4.99€" button="Add"></Product>
+      <Product img="" title="Brown Sugar" price="4.99€" button="Add"></Product>
+      <Product img="" title="Brown Sugar" price="4.99€" button="Add"></Product>
+      <Product img="" title="Brown Sugar" price="4.99€" button="Add"></Product>
+      <Product img="" title="Brown Sugar" price="4.99€" button="Add"></Product>
+      <Product img="" title="Brown Sugar" price="4.99€" button="Add"></Product>
     </div>
   );
 }
