a1407ad1fc585c45981f8a60d22ac78b6ef4f720
Mac <sophanpa@gmail.com>
2023-06-18 18:17:44 +0200

    Add next and previous button functions, displaying pokemons and loading img

--- bubble_tea_store/src/components/Product.tsx
+++ bubble_tea_store/src/components/Product.tsx
@@ -1,9 +1,43 @@
+import { useState } from 'react';
+
 function Product(props) {
+  const [pic, setPic] = useState('assets/lol.png');
+
+  const [num, setNum] = useState(1);
+  const next = () => {
+    setNum(num + 1);
+    setPic('assets/lol.png');
+  };
+  const previous = () => {
+    setNum(num - 1);
+    setPic('assets/lol.png');
+  };
+  fetch('https://pokeapi.co/api/v2/pokemon/' + num, {
+    mode: 'cors',
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      console.log('lol');
+      console.log(response);
+
+      setPic(response.sprites.front_default);
+    });
+
   return (
     <div id="Product">
-      <img src="assets/lol.png" alt="" />
-      {<button id="back">{props.BackButton}</button>}
-      {<button id="forward">{props.ForwardButton}</button>}
+      <img id="lol" src={pic} alt="pic" />
+      {
+        <button id="back" onClick={previous}>
+          {props.BackButton}
+        </button>
+      }
+      {
+        <button id="forward" onClick={next}>
+          {props.ForwardButton}
+        </button>
+      }
     </div>
   );
 }
